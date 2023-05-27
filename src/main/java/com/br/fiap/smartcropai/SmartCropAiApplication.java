a9113226c9826a4257e1931b75467b96626383a4
Vinícius Yoda <vyoda4604@gmail.com>
2023-05-27 17:18:34 -0300

    Initial commit

---
+++ src/main/java/com/br/fiap/smartcropai/SmartCropAiApplication.java
@@ -0,0 +1,13 @@
+package com.br.fiap.smartcropai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCropAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCropAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/fiap/smartcropai/SmartCropAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fiap.smartcropai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCropAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
