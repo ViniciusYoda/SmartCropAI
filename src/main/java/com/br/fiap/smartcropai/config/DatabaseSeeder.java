8bbd5fd1c0ad4358f8e31c58abd6a4e11e116800
Vinícius Yoda <vyoda4604@gmail.com>
2023-08-19 13:07:49 -0300

    up

--- src/main/java/com/br/fiap/smartcropai/config/DatabaseSeeder.java
+++ src/main/java/com/br/fiap/smartcropai/config/DatabaseSeeder.java
@@ -1,53 +1,53 @@
-package com.br.fiap.smartcropai.config;
+// package com.br.fiap.smartcropai.config;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.context.annotation.Configuration;
 
-import com.br.fiap.smartcropai.models.Solo;
-import com.br.fiap.smartcropai.models.Clima;
-import com.br.fiap.smartcropai.models.Usuario;
-import com.br.fiap.smartcropai.repository.SoloRepository;
-import com.br.fiap.smartcropai.repository.ClimaRepository;
-import com.br.fiap.smartcropai.repository.UsuarioRepository;
+// import com.br.fiap.smartcropai.models.Solo;
+// import com.br.fiap.smartcropai.models.Clima;
+// import com.br.fiap.smartcropai.models.Usuario;
+// import com.br.fiap.smartcropai.repository.SoloRepository;
+// import com.br.fiap.smartcropai.repository.ClimaRepository;
+// import com.br.fiap.smartcropai.repository.UsuarioRepository;
 
-@Configuration
-public class DatabaseSeeder implements CommandLineRunner {
+// @Configuration
+// public class DatabaseSeeder implements CommandLineRunner {
 
-   @Autowired
-   SoloRepository soloRepository;
+//    @Autowired
+//    SoloRepository soloRepository;
 
-   @Autowired
-   ClimaRepository climaRepository;
+//    @Autowired
+//    ClimaRepository climaRepository;
 
-   @Autowired 
-   UsuarioRepository usuarioRepository;
+//    @Autowired 
+//    UsuarioRepository usuarioRepository;
 
-   @Override
-   public void run(String... args) throws Exception {
-      soloRepository.saveAll(List.of(
-         Solo.builder().texturaDoSolo("Argiloso").phDoSolo(6.5).retencaoDeAgua("Alta").condutEletrica("Cobre").build(),
-         Solo.builder().texturaDoSolo("Arenoso").phDoSolo(4.8).retencaoDeAgua("Alta").condutEletrica("Cobre").build(),
-         Solo.builder().texturaDoSolo("Humoso").phDoSolo(8.8).retencaoDeAgua("Alta").condutEletrica("Cobre").build(),
-         Solo.builder().texturaDoSolo("Argiloso").phDoSolo(7).retencaoDeAgua("Baixa").condutEletrica("Cobre").build(),
-         Solo.builder().texturaDoSolo("Arenoso").phDoSolo(3.9).retencaoDeAgua("Baixa").condutEletrica("Cobre").build(),
-         Solo.builder().texturaDoSolo("Humoso").phDoSolo(10).retencaoDeAgua("Alta").condutEletrica("Cobre").build()
-      ));
-      climaRepository.saveAll(List.of(
-         Clima.builder().cep("02945-100").precipitacao("Baixa").umidade("Baixa").velDoVento("Ventos alisios").dirDoVento("Moderados a fortes").pressaoAtmosferica("Alta").build(),
-         Clima.builder().cep("69907-290").precipitacao("Alta").umidade("Baixa").velDoVento("Ventos alisios").dirDoVento("Moderados a baixo").pressaoAtmosferica("Baixo").build(),
-         Clima.builder().cep("69313-458").precipitacao("Baixa").umidade("Alta").velDoVento("Ventos alisios").dirDoVento("Fortes").pressaoAtmosferica("Alta").build(),
-         Clima.builder().cep("79073-245").precipitacao("Alta").umidade("Alta").velDoVento("Ventos alisios").dirDoVento("Baixos").pressaoAtmosferica("Baixo").build()
-      ));
-      usuarioRepository.save(Usuario.builder()
-      .nome("José Maria")
-      .cpf("360.121.618-32")
-      .email("jose.marinha@gmail.com")
-      .senha("$2a$12$pMH3uGhwRXAaEq21jmmqn.PzxykI/HJyVAXM6sIQlcQ/2emqevaWC")
-      .build()
-      );
-   }
+//    @Override
+//    public void run(String... args) throws Exception {
+//       soloRepository.saveAll(List.of(
+//          Solo.builder().texturaDoSolo("Argiloso").phDoSolo(6.5).retencaoDeAgua("Alta").condutEletrica("Cobre").build(),
+//          Solo.builder().texturaDoSolo("Arenoso").phDoSolo(4.8).retencaoDeAgua("Alta").condutEletrica("Cobre").build(),
+//          Solo.builder().texturaDoSolo("Humoso").phDoSolo(8.8).retencaoDeAgua("Alta").condutEletrica("Cobre").build(),
+//          Solo.builder().texturaDoSolo("Argiloso").phDoSolo(7).retencaoDeAgua("Baixa").condutEletrica("Cobre").build(),
+//          Solo.builder().texturaDoSolo("Arenoso").phDoSolo(3.9).retencaoDeAgua("Baixa").condutEletrica("Cobre").build(),
+//          Solo.builder().texturaDoSolo("Humoso").phDoSolo(10).retencaoDeAgua("Alta").condutEletrica("Cobre").build()
+//       ));
+//       climaRepository.saveAll(List.of(
+//          Clima.builder().cep("02945-100").precipitacao("Baixa").umidade("Baixa").velDoVento("Ventos alisios").dirDoVento("Moderados a fortes").pressaoAtmosferica("Alta").build(),
+//          Clima.builder().cep("69907-290").precipitacao("Alta").umidade("Baixa").velDoVento("Ventos alisios").dirDoVento("Moderados a baixo").pressaoAtmosferica("Baixo").build(),
+//          Clima.builder().cep("69313-458").precipitacao("Baixa").umidade("Alta").velDoVento("Ventos alisios").dirDoVento("Fortes").pressaoAtmosferica("Alta").build(),
+//          Clima.builder().cep("79073-245").precipitacao("Alta").umidade("Alta").velDoVento("Ventos alisios").dirDoVento("Baixos").pressaoAtmosferica("Baixo").build()
+//       ));
+//       usuarioRepository.save(Usuario.builder()
+//       .nome("José Maria")
+//       .cpf("360.121.618-32")
+//       .email("jose.marinha@gmail.com")
+//       .senha("$2a$12$pMH3uGhwRXAaEq21jmmqn.PzxykI/HJyVAXM6sIQlcQ/2emqevaWC")
+//       .build()
+//       );
+//    }
    
-}
+// }
